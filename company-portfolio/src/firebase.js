1012dd22aef793b4983793967ff4d4efbd2fed24
Daniyal Sohail <dsohail65@gmail.com>
2023-10-26 00:24:28 -0400

    contact_form

--- company-portfolio/src/firebase.js
+++ company-portfolio/src/firebase.js
@@ -24,21 +24,23 @@ const app = initializeApp(firebaseConfig);
   export { db };*/
 
   //import firebase from 'firebase';
-  import { initializeApp } from 'firebase/app';
-  import { getFirestore } from 'firebase/firestore';
-  const firebaseConfig = {
-  //var firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAP9RPFp6U5_O63lAyhp1e_E80IfZyTIJY",
-    authDomain: "versa-web-design-71956.firebaseapp.com",
-    projectId: "versa-web-design-71956",
-    storageBucket: "versa-web-design-71956.appspot.com",
-    messagingSenderId: "963275800079",
-    appId: "1:963275800079:web:12facecd6f770a473bad7a"
-  };
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-  //var db = firebaseApp.firestore();
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  console.log('Firestore database:', db);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvFpRPA-PK4CQRcFRSudCCgqc5u23sfYg",
+  authDomain: "versa-web-design-7a546.firebaseapp.com",
+  projectId: "versa-web-design-7a546",
+  storageBucket: "versa-web-design-7a546.appspot.com",
+  messagingSenderId: "651038715404",
+  appId: "1:651038715404:web:c454ac2085624b7262c829"
+};
 
-  export { db };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db };
